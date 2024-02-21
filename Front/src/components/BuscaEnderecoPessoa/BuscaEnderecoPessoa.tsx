ec254949d4d73c1516bc9c2e6f9e9bca99eeb6a6
Igor Fronza <igorfronza20@gmail.com>
2024-02-20 21:25:53 -0300

    Falta Corrigir

--- Front/src/components/BuscaEnderecoPessoa/BuscaEnderecoPessoa.tsx
+++ Front/src/components/BuscaEnderecoPessoa/BuscaEnderecoPessoa.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 
 interface buscaPessoaEndereco {
@@ -21,25 +22,28 @@ function BuscaEnderecoPessoa() {
         try {
             const EnderecoPessoa = await axios.get(`http://localhost:5000/pessoa/endereco/${id}`);
             console.log('Dados retornados:', EnderecoPessoa.data);
-            setPessoaEndereco(EnderecoPessoa.data);
-            console.log("Teste: ",PessoaEndereco);
+            setPessoaEndereco(EnderecoPessoa.data[id]);
+            console.log("Teste: ", PessoaEndereco);
         } catch (error) {
             alert('Não foi possível encontrar o Endereço da pessoa');
         }
     }
 
     return (
-        <form action="" onSubmit={handleSubmit} className="form">
-            <input type="number" value={id} onChange={(e) => setId(e.target.value)} placeholder="Digite o ID" />
-            <button type='submit'>Pesquisar</button>
-            {PessoaEndereco && (
-                <div className="results">
-                    <h2>Resultados Encotrados:</h2>
-                    <p>Bairro: {PessoaEndereco.end_bairro}</p>
-                    <p>Cidade: {PessoaEndereco.end_cidade}</p>
-                </div>
-            )}
-        </form>
+        <>
+            <form action="" onSubmit={handleSubmit} className="form">
+                <input type="number" value={id} onChange={(e) => setId(e.target.value)} placeholder="Digite o ID" />
+                <button type='submit'>Pesquisar</button>
+                {PessoaEndereco && (
+                    <div className="results">
+                        <h2>Resultados Encotrados:</h2>
+                        <p>Bairro: {PessoaEndereco.end_bairro}</p>
+                        <p>Cidade: {PessoaEndereco.end_cidade}</p>
+                    </div>
+                )}
+            </form>
+            <button><Link to={'/'}>Voltar para a Pagina Inicial</Link></button>
+        </>
     )
 }
 
